d4ea4490bae3bd6ea001d4c3b191370aed3b0e9d
JoaoPGH <suporteti.joao2021@hotmail.com>
2025-04-23 21:22:05 -0300

    Corrigindo extração do campo Port utilizando atributo data-port no HTML

--- WebCrawlerProxy/Services/HtmlParserService.cs
+++ WebCrawlerProxy/Services/HtmlParserService.cs
@@ -20,12 +20,13 @@ namespace WebCrawlerProxy.Services
                 foreach (var row in rows)
                 {
                     var cells = row.SelectNodes("td");
-                    if (cells?.Count >= 4)
+                    
+                    if (cells?.Count >= 7)
                     {
                         proxies.Add(new ProxyData
                         {
                             Ip = cells[1].InnerText.Trim(),
-                            Port = cells[2].InnerText.Trim(),
+                            Port = cells[2].SelectSingleNode(".//span[@class='port']")?.GetAttributeValue("data-port", "").Trim(),
                             Country = cells[3].InnerText.Trim(),
                             Protocol = cells[6].InnerText.Trim()
                         });
